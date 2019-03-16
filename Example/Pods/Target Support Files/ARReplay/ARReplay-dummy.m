e795b2cafaccd7d30dd665a6344acbc62dd088bf
Yang Zhang <zhangy405@gmail.com>
2019-03-16 13:49:09 -0700

    init

---
+++ Example/Pods/Target Support Files/ARReplay/ARReplay-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ARReplay : NSObject
+@end
+@implementation PodsDummy_ARReplay
+@end
---
+++ Example/Pods/Target Support Files/Pods-ARReplay_Example/Pods-ARReplay_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ARReplay_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ARReplay_Example
+@end
